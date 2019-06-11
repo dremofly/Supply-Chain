1488efc6f7de1daa7594ef7be770ad93f39fe7ba
dremofly <dremofly@gmail.com>
2019-06-11 20:53:34 +0800

    新的界面在ganacha gui上测试可用

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -3,14 +3,14 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 8545,
+      port: 7545,
       network_id: "*" // Match any network id
-    },
+    },/*
     rinkeby: {
     provider: () => new HDWalletProvider(<seed>, <link></link>), // metamask seed, a link from fura
       network_id: 4, 
       gas: 4500000,
       gasPrice: 10000000000
-    },
+    },*/
   }
 };
